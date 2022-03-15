2c3b7220c3689461720fb33b158217e440332e2a
Regtables <reghardt7@gmail.com>
2022-03-15 15:43:36 +0200

    completed CTA and Footer sections

--- src/Containers/CTABig/CTABig.js
+++ src/Containers/CTABig/CTABig.js
@@ -4,8 +4,18 @@ import './CTABig.css'
 
 const CTABig = () => {
   return (
-    <div>
-      
+    <div className = 'hyperfund__cta section__padding'>
+        <div className = 'hyperfund__cta-heading'>
+            <h2>Register Today</h2>
+        </div>
+        <div className = 'hyperfund__cta-content'>
+            <div className = 'hyperfund__cta-content_text'>
+                <p>Suspendisse egestas rhoncus urna in bibendum. Donec urna neque, euismod sed justo quis, convallis tincidunt risus. Phasellus fringilla odio vel erat fringilla, </p>
+            </div>
+            <div className = 'hyperfund__cta-content_btn'>
+                <p>Register Now</p>
+            </div>
+        </div>
     </div>
   )
 }
--- src/Containers/footer/Footer.js
+++ src/Containers/footer/Footer.js
@@ -4,8 +4,14 @@ import './footer.css'
 
 const Footer = () => {
   return (
-    <div>
-      
+    <div className = 'hyperfund__footer section__padding'>
+        <div className = 'hyperfund__footer-top'>
+            <p id = 'first'>Terms of Service</p>
+            <p>Privacy Policy</p>
+        </div>
+        <div className = 'hyperfund__footer-bottom'>
+            <p>Ⓒ Hyperfund Downline, 2022</p>
+        </div>
     </div>
   )
 }
